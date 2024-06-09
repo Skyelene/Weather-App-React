6b25d7ebebed0df18fb1e214e2566cb43e5e9fd3
Rach <152854614+Skyelene@users.noreply.github.com>
2024-06-09 11:10:22 +0200

    Added footer

--- src/WeatherSearch.js
+++ src/WeatherSearch.js
@@ -57,6 +57,25 @@ export default function WeatherSearch() {
   }
 
   <footer>
-    Coded by Rachel Buday, Open-sourced on GitHub and hosted on Netlify.
+    Coded by
+    <a href="https://github.com/Skyelene" target="_blank" rel="noreferrer">
+      Rachel Buday,
+    </a>
+    Open-sourced on
+    <a
+      href="https://github.com/Skyelene/Weather-App-React"
+      target="_blank"
+      rel="noreferrer"
+    >
+      GitHub
+    </a>
+    and hosted on
+    <a
+      href="https://wetter-app-mit-react.netlify.app/"
+      target="_blank"
+      rel="noreferrer"
+    >
+      Netlify.
+    </a>
   </footer>;
 }
