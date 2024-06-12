2ac8f24684632ee9e61c6112cfe6c8e27b202770
Rach <152854614+Skyelene@users.noreply.github.com>
2024-06-12 10:58:20 +0200

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -5,8 +5,36 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <Weather />
+      <div className="container">
+        <h1>Weather App</h1>
+        <Weather />
+        <footer>
+          Coded by{" "}
+          <a
+            href="https://github.com/Skyelene"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Rachel Buday,
+          </a>{" "}
+          Open-sourced on{" "}
+          <a
+            href="https://github.com/Skyelene/Weather-App-React"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>{" "}
+          and hosted on{" "}
+          <a
+            href="https://wetter-app-mit-react.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Netlify.
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
--- src/WeatherSearch.js
+++ src/WeatherSearch.js
@@ -50,28 +50,6 @@ export default function WeatherSearch() {
           </li>
         </ul>
       )}
-      <footer>
-        Coded by{" "}
-        <a href="https://github.com/Skyelene" target="_blank" rel="noreferrer">
-          Rachel Buday,
-        </a>{" "}
-        Open-sourced on{" "}
-        <a
-          href="https://github.com/Skyelene/Weather-App-React"
-          target="_blank"
-          rel="noreferrer"
-        >
-          GitHub
-        </a>{" "}
-        and hosted on{" "}
-        <a
-          href="https://wetter-app-mit-react.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Netlify.
-        </a>
-      </footer>
     </div>
   );
 }
